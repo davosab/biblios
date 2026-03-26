6324cdf79e4fd79104ac25395127bdf5d0890972
davosab <davidsabau72@gmail.com>
2026-03-26 11:47:24 +0200

    clean

--- src/main.js
+++ src/main.js
@@ -4,6 +4,5 @@ import './style.css'
 
 import router from './router/router'
 
-
 const app = createApp(App)
 app.use(router).mount('#app');
--- src/router/router.js
+++ src/router/router.js
@@ -1,47 +1,12 @@
-import {createRouter, createWebHashHistory} from 'vue-router'
-import Patients from '../pages/Patients/Patients.vue'
-import Invoices from '../pages/Invoices/Invoices.vue'
-import Inventory from '../pages/Inventory/Inventory.vue'
-import Appointments from '../pages/Appointments/Appointments.vue'
-import Wards from '../pages/Wards/Wards.vue'
-import AddPatientPage from '../pages/Patients/AddPatientPage.vue'
-import AddAppointmentPage from '../pages/Appointments/AddAppointmentPage.vue'
-import EditAppointmentPage from '../pages/Appointments/EditAppointment.vue'
-import AddInventoryPage from '../pages/Inventory/AddInventoryPage.vue'
-import EditInventoryPage from '../pages/Inventory/EditInventoryPage.vue'
-import EditPatientPage from '../pages/Patients/EditPatientPage.vue'
-import AddInvoicePage from '../pages/Invoices/AddInvoicePage.vue'
-import EditInvoicePage from '../pages/Invoices/EditInvoicePage.vue'
-import AddWardPage from '../pages/Wards/AddWardPage.vue'
-import EditWardPage from '../pages/Wards/EditWardPage.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
-    {path:'/', redirect:'/patients'},
-    {path:'/patients', name:'patients', component: Patients},
-    {path:'/invoices', name:'invoices', component: Invoices},
-    {path:'/inventory', name:'inventory', component: Inventory},
-    {path:'/appointments', name:'appointments', component: Appointments},
-    {path:'/wards', name:'wards', component: Wards},
-    {path:'/addPatient', name:'addPatient', component: AddPatientPage},
-    {path:'/editPatient', name:'editPatient', component: EditPatientPage},
-    {path:'/addAppointment', name:'addAppointment', component: AddAppointmentPage},
-    {path:'/editAppointment', name:'editAppointment', component: EditAppointmentPage},
-    {path:'/addInventory', name:'addInventory', component: AddInventoryPage},
-    {path:'/editInventory', name:'editInventory', component: EditInventoryPage},
-    {path:'/addInvoice', name:'addInvoice', component: AddInvoicePage},
-    {path:'/editInvoice', name:'editInvoice', component: EditInvoicePage},
-    {path:'/addWard', name:'addWard', component: AddWardPage},
-    {path:'/editWard', name:'editWard', component: EditWardPage},
-    
-
-
-
-
+    // {path:'/', redirect:'/patients'}
 ]
 
-  const router = createRouter({
-        history: createWebHashHistory('/SistemSpital/'),
-        routes
-    })
+const router = createRouter({
+    history: createWebHashHistory('/SistemSpital/'),
+    routes
+})
 
 export default router
